40281fd6739287371a66f0f37f68dad851584f7f
myHan <hmy3743@gmail.com>
2020-01-06 17:44:16 +0900

    setup websocket

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+'use strict';
+const Message = require('./protobuf/message_pb').Message;
+const Express = require('express');
+const Https = require('https');
+const WebSocket = require('ws');
+const FileSystem = require('fs');
+const app = Express();
 
-io.on('connection', client => {
-    console.log(`connect ${client.id}`);
-    client.on('disconnect', () => {
-        console.log(`disconnect ${client.id}`);
+app.use(function (req, res) {
+    res.send({ msg: 'hello' });
+});
+
+const server = Https.createServer({
+    key: FileSystem.readFileSync('key.pem'),
+    cert: FileSystem.readFileSync('cert.pem'),
+    passphrase: 'pass'
+}, app);
+
+const wss = new WebSocket.Server({ server })
+wss.on('connection', function connection(socket) {
+    socket.on('message', function incoming(bytes) {
+        const message = Message.deserializeBinary(bytes);
+        const type = message.getType();
+
+        if(typeof this.mapping == 'undefined'){
+            this.mapping = new Map()
+                .set('chat/plainText', (message) => {
+                    const bytes = message.serializeBinary();
+                    wss.clients.forEach((item) => {
+                        item.send(bytes);
+                    });
+                });
+        }
+
+        this.mapping.get(type)(message);
     });
 });
-
-server.listen(5000, () => {
-    console.log('Server is listen');
+server.listen(5000, function listening() {
+    console.log('Listening on %d', server.address().port);
 });
-
 exports.server = server;
--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,70 @@
 const should = require('should');
-const client = require('socket.io-client');
+const ws = require('ws');
+const Message = require('../src/protobuf/message_pb').Message;
 
-const socketURL = "http://localhost:5000";
+const socketURL = "wss://localhost:5000";
 const options = {
-    transports: ['websocket'],
-    'force new connection': true
+    origin: 'https://localhost:5000'
 };
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 describe('Server', () => {
     let server;
-    beforeEach('Start server', done => {
+    before('Setting server', done => {
         server = require('../src/index').server;
-        done();
-    });
-
-    afterEach('Close server', done => {
         server.close();
         done();
     });
 
+    beforeEach('Start server', done => {
+        server.listen(5000, function listening() {
+            done();
+        });
+    });
+
+    afterEach('Close server', done => {
+        server.close((err) => {
+            done();
+        });
+    });
+
     describe('Socket', () => {
         it('Should connectable', (done) => {
-            const socket = client.connect(socketURL, options);
-            socket.on('connect', () => {
-                done();
+            const socket = new ws(socketURL, options);
+            socket.on('open', () => {
                 socket.close();
+                done();
+            });
+        });
+    });
+
+    describe('message', () => {
+        it('should broadcast message', (done) => {
+            const owner = 'unittest-owner';
+            const content = 'unittest-content';
+            const type = 'chat/plainText';
+            
+            const client = new ws(socketURL, options);
+            client.on('message', (data, flags) => {
+                const message = Message.deserializeBinary(data);
+                message.getOwner().should.equal(owner);
+                message.getContent().should.equal(content);
+                message.getType().should.equal(type);
+                
+                client.close();
+                
+                done();
+            });
+            client.on('open', () => {
+                const message = new Message();
+                message.setOwner(owner);
+                message.setContent(content);
+                message.setType(type);
+                
+                const bytes = message.serializeBinary();
+                
+                client.send(bytes);
             });
         });
     });
